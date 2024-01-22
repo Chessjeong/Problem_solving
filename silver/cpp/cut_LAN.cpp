efee49928b95e87da213d378752f89629d8b4444
Minho <jmhoo0618@kaist.ac.kr>
2024-01-22 21:59:35 +0900

    unsolved

--- silver/cpp/cut_LAN.cpp
+++ silver/cpp/cut_LAN.cpp
@@ -1,1 +1,26 @@
 #include <iostream>
+#include <numeric>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int K, N;
+    cin >> K >> N;
+    int origin[K];
+    for(int i = 0; i< K; i++){
+        cin >> origin[i];
+    }
+    int sum = accumulate(origin, origin+K,0);
+    int p = (sum/N)+1;
+    int q[K];
+    int q_sum=0;
+    while(true){
+        q_sum = 0;
+        for(int i = 0; i < K; i++) q[i] = origin[i]/p;
+        q_sum = accumulate(q, q+K, 0);
+        if(q_sum >= N) break;
+        --p;
+    }
+    cout << p << endl;
+}
