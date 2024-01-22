0e04a33a86e800aa343a741053ba7cd08ac93086
Minho <jmhoo0618@kaist.ac.kr>
2024-01-22 22:09:44 +0900

    solved

--- bronze/cpp/gcd.cpp
+++ bronze/cpp/gcd.cpp
@@ -2,12 +2,28 @@
 using namespace std;
 
 int compute_gcd(int a,int b){
+    int q = a/b;
+    int r = a%b;
+    if(r == 0) return b;
+    else return compute_gcd(b,r);
+}
 
+int compute_lcm(int a, int b){
+    return a*b/compute_gcd(a,b);
 }
 
 int main(){
     int a, b;
     cin >> a >> b;
-
+    if(a<b){
+        int tmp ;
+        tmp = a;
+        a = b;
+        b = tmp;
+    }
+    int gcd = compute_gcd(a,b);
+    int lcm = compute_lcm(a,b);
+    cout << gcd << endl;
+    cout << lcm << endl;
 
 }
