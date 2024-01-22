0a14017059af4e5567134d451637f5f18a62db47
Minho <jmhoo0618@kaist.ac.kr>
2024-01-22 21:10:07 +0900

    solved

--- silver/cpp/sugar_deliver.cpp
+++ silver/cpp/sugar_deliver.cpp
@@ -1,1 +1,33 @@
 #include <iostream>
+#include <algorithm>
+
+using namespace std;
+int ans[5001];
+
+int deliver(int target) {
+    if (target < 3) return -1;
+    if (target == 3 || target == 5) return 1;
+    if (target == 4) return -1;
+
+    fill(ans, ans + target + 1, -1); // Fill the array up to the target
+    ans[3] = 1;
+    ans[5] = 1;
+
+    for (int i = 6; i <= target; ++i) {
+        if (i >= 3 && ans[i-3] != -1) ans[i] = ans[i-3] + 1;
+        if (i >= 5 && ans[i-5] != -1) {
+            if (ans[i] == -1 || ans[i-5] + 1 < ans[i]) {
+                ans[i] = ans[i-5] + 1;
+            }
+        }
+    }
+
+    return ans[target];
+}
+
+int main(){
+    int target;
+    cin >> target;
+    int res = deliver(target);
+    cout << res<< endl;
+}
