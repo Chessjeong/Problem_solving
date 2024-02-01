39e0766e8bd03d00b3a89321de215fa8599b1837
Minho <jmhoo0618@kaist.ac.kr>
2024-02-01 23:37:32 +0900

    solved

--- silver/cpp/cut_LAN.cpp
+++ silver/cpp/cut_LAN.cpp
@@ -1,26 +1,37 @@
 #include <iostream>
 #include <numeric>
-#include <cmath>
+#include <algorithm>
+#include <vector>
 
 using namespace std;
 
-int main(){
+int main() {
     int K, N;
     cin >> K >> N;
-    int origin[K];
-    for(int i = 0; i< K; i++){
-        cin >> origin[i];
+    vector<long long> wires(K);
+    for(int i = 0; i < K; ++i) {
+        cin >> wires[i];
     }
-    int sum = accumulate(origin, origin+K,0);
-    int p = (sum/N)+1;
-    int q[K];
-    int q_sum=0;
-    while(true){
-        q_sum = 0;
-        for(int i = 0; i < K; i++) q[i] = origin[i]/p;
-        q_sum = accumulate(q, q+K, 0);
-        if(q_sum >= N) break;
-        --p;
+
+    long long left = 1, right = *max_element(wires.begin(), wires.end());
+    long long answer = 0;
+
+    while (left <= right) {
+        long long mid = left + (right - left) / 2;
+        long long count = 0;
+
+        for (int i = 0; i < K; ++i) {
+            count += wires[i] / mid;
+        }
+
+        if (count >= N) {
+            answer = mid;
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
     }
-    cout << p << endl;
+
+    cout << answer << endl;
+    return 0;
 }
