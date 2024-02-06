f1eb6ded1c7a9cb22143d7633fb0e70719025f6a
Minho <jmhoo0618@kaist.ac.kr>
2024-02-06 17:47:51 +0900

    solved

--- gold/cpp/min_max.cpp
+++ gold/cpp/min_max.cpp
@@ -9,13 +9,32 @@ class Node{
         Node* left;
         Node* right;
         int value;
-        Node(int n){
+        Node(int n=0){
             value(n);
             parent=NULL;
             left=NULL;
             right=NULL;
         }        
 };
+
+Node min(Node a, Node b){
+    Node ret;
+    if(a.value <= b.value){
+        ret.left
+    }
+}
+
+int find_min(vector<Node> sequence){
+    if(sequence.size() < 2) return sequence[0].value;
+    int middle = sequence.size()/2
+    vector<Node> front(sequence.begin(),sequence.begin()+middle);
+    vector<Node> back(sequence.begin()+middle,sequence.end())
+}
+
+int find_max(vector<Node> sequence){
+
+}
+
 
 int main(){
     ios::sync_with_stdio(false);
@@ -29,13 +48,16 @@ int main(){
     for(int i = 0; i<N; i++){
         int num;
         cin >> num;
-        min_parent[i]=num;
-        max_parent[i]=num;
+        Tree input(num);
+        sequence.push_back(input);
     }
     for(int i = 0; i<M; i++){
         int a, b;
         cin >> a >> b;
-
+        vector<Node> partial(sequence.begin()+a-1,sequence.begin()+b);
+        int min = find_min(partial);
+        int max = find_max(partial);
+        output.push_back(make_pair(min,max));
     }
-
+    for(int i = 0; i<output.size(); i++) cout << output[i].first << " " << output[i].second << endl;
 }
